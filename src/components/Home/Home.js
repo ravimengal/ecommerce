225b6ed060adc8602fbdfd438c4306a30be2e8c0
ravimengal <mengal22ravi@gmail.com>
2022-01-19 15:44:06 +0530

    Adding routes

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return
+  
+  <div className='container'>
+
+        <h1>Buy some awesome products</h1>
+        <h3>FakeStore fetching data from Product api</h3>
+
+
+  </div>;
+};
+
+export default Home;
