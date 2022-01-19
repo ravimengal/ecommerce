7ff6c749a64256e1b95b5730758f9ccc28b3ae99
ravimengal <mengal22ravi@gmail.com>
2022-01-19 15:43:31 +0530

    Adding more routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Fetching from './components/Fetching';
 import Nav from './components/Nav';
+import Home from './components/Home/Home'
 import {BrowserRouter as Router , Route , Switch, Link} from 'react-router-dom'
 
 
@@ -9,10 +10,10 @@ function App() {
   return (
     <Router>
       <div>
+        <Nav/>
         <Switch>
           <Route path="/nav" component={Nav} />
-           
-         
+          <Route path="/home" component={Home}/>
         </Switch>
         <Fetching />
       </div>
